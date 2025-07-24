d8dc4948981a14cc06dff6c8feaba57d095a1ea5
TAMILARASAN M <tamilarasanmuruganofficial@gmail.com>
2025-07-24 22:33:21 +0530

    Remove AWS secrets and secure property config

--- Serenique Hotel/Backend/src/main/java/com/Sereniquedev/SereniqueHotel/security/JWTAuthFilter.java
+++ Serenique Hotel/Backend/src/main/java/com/Sereniquedev/SereniqueHotel/security/JWTAuthFilter.java
@@ -1,13 +1,17 @@
 package com.Sereniquedev.SereniqueHotel.security;
 
+import com.Sereniquedev.SereniqueHotel.service.CustomUserDetailsService;
 import com.Sereniquedev.SereniqueHotel.utils.JWTUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.CachingUserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,7 +24,7 @@ public class JWTAuthFilter extends OncePerRequestFilter {
     private JWTUtils jwtUtils;
 
     @Autowired
-    private CachingUserDetailsService cachingUserDetailsService;
+    private CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -35,5 +39,18 @@ public class JWTAuthFilter extends OncePerRequestFilter {
         }
 
         jwtToken=authHeader.substring(7);
+        userEmail = jwtUtils.extractUsername(jwtToken);
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
+            if (jwtUtils.isValidToken(jwtToken, userDetails)) {
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                securityContext.setAuthentication(token);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
